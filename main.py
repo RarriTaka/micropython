01718f4d04aad0e1d056a84b07bbb2ef939d0f19
Ronel Samangua <109470954+RarriTaka@users.noreply.github.com>
2024-09-10 15:21:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ def on_off():
 
 while True:
     switch.callback(on_off)
+
+///////////////////////////////////////////////////////////////////////////
+from pyb import Timer, Pin, ADC
+import time
+
+my_pin = pyb.Pin( 'X1', pyb.Pin.OUT_PP)
+my_pin2 = pyb.Pin( 'X6', pyb.Pin.OUT_PP)
+
+while True:
+    my_pin.high()
+    time.sleep(1)
+    my_pin.low()
+    time.sleep(1)
