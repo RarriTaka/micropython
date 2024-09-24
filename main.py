8f9f0160a6b0a1cdb9bcd4d4e2f3984295391cf4
Ronel Samangua <109470954+RarriTaka@users.noreply.github.com>
2024-09-24 15:07:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,3 +21,21 @@ while True:
     time.sleep(1)
     my_pin.low()
     time.sleep(1)
+///////////////////////////////////////////////////////////////////////////
+from pyb import Timer, Pin, ADC
+import time
+
+timer = pyb.Timer(5, freq=500)
+channel_bleu = timer.channel(3, Timer.PWM, pin=Pin('X1'), pulse_width_percent=100)
+channel_rouge = timer.channel(4, Timer.PWM, pin=Pin('X2'), pulse_width_percent=100)
+
+while True:
+    for i in range(101):
+        channel_bleu.pulse_width_percent(i)
+        channel_rouge.pulse_width_percent(100-i)
+        time.sleep(0.1)
+    
+    for i in range(101):
+        channel_bleu.pulse_width_percent(100-i)
+        channel_rouge.pulse_width_percent(i)
+        time.sleep(0.1)
